0f121be06516647bda4eac42b00da849cad8a0c9
iamjunsulee <mellow0924@gmail.com>
2017-11-16 18:26:49 +0900

    application of thymeleaf

---
+++ src/main/java/me/jjunsu/index/IndexController.java
@@ -0,0 +1,22 @@
+package me.jjunsu.index;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import me.jjunsu.post.PostRepository;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+	private final PostRepository postRepository;
+	
+	@GetMapping("/")
+	public String home(Model model, Pageable pageable){
+		model.addAttribute("posts", postRepository.findAll(pageable));
+		//model.addAttribute("message", "hello world");
+		return "index";
+	}
+}
